7b2d75e490f086be6865308b9df44c6c44f2d057
geneaky <kaktus418@gmail.com>
2021-12-30 17:20:30 +0900

    [refact] apply frontcontroller v5 - v4

--- src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -10,6 +10,7 @@ import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
 import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
 import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -32,14 +33,19 @@ public class FrontControllerServletV5 extends HttpServlet {
     initHandlerAdapters();
   }
 
-  private boolean initHandlerAdapters() {
-    return handlerAdapters.add(new ControllerV3HandlerAdapter());
+  private void initHandlerAdapters() {
+    handlerAdapters.add(new ControllerV3HandlerAdapter());
+    handlerAdapters.add(new ControllerV4HandlerAdapter());
   }
 
   private void initHandlerMappingMap() {
     handlerMappingMap.put("/front-controller/v5/v3/members/new-form",new MemberFormControllerV3());
     handlerMappingMap.put("/front-controller/v5/v3/members/save",new MemberSaveControllerV3());
     handlerMappingMap.put("/front-controller/v5/v3/members",new MemberListControllerV3());
+
+    handlerMappingMap.put("/front-controller/v5/v4/members/new-form",new MemberFormControllerV4());
+    handlerMappingMap.put("/front-controller/v5/v4/members/save",new MemberSaveControllerV4());
+    handlerMappingMap.put("/front-controller/v5/v4/members",new MemberListControllerV4());
   }
 
   @Override
